f95ec24886b8645bc810fae511492a53a1d0ed0e
Nicolas <nigarcia88@gmail.com>
2018-02-13 20:20:54 +0100

    new: testPanelControl

--- TestingPushNotificationsUITests/Resources/SpringBoardActionableTextInputNotification.swift
+++ TestingPushNotificationsUITests/Resources/SpringBoardActionableTextInputNotification.swift
@@ -1,15 +1,15 @@
 //
-//  SpringBoardActionableNotification.swift
+//  SpringBoardActionableTextInputNotification.swift
 //  TestingPushNotificationsUITests
 //
-//  Created by Nicolás García on 11-02-18.
+//  Created by Nicolás García on 13-02-18.
 //  Copyright © 2018 Nicolas Garcia. All rights reserved.
 //
 
 import Foundation
 import XCTest
 
-struct SpringBoardActionableNotification {
+struct SpringBoardActionableTextInputNotification {
     
     let springboard: XCUIApplication
     
@@ -17,14 +17,15 @@ struct SpringBoardActionableNotification {
         return springboard.buttons["TESTINGPUSHNOTIFICATIONS"].firstMatch
     }
     var messageButton: XCUIElement {
-        return springboard.buttons["Your bike was stolen!"].firstMatch
+        return springboard.buttons["You have a message!"].firstMatch
     }
     var dismissButton: XCUIElement {
         return springboard.buttons["Dismiss"].firstMatch
     }
-    var actionButton: XCUIElement {
-        return springboard.buttons["ACTION"].firstMatch
+    var sendButton: XCUIElement {
+        return springboard.buttons["Send"].firstMatch
+    }
+    var textField: XCUIElement {
+        return springboard.textFields["Add Comment Here"].firstMatch
     }
 }
-
-
